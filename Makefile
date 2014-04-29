4ed41c644f0e1d9e21b566673077d69cb0a19a9a
Nikolay Podstrigan <parikmaher@yandex-team.ru>
2014-04-29 23:13:01 +0300

    Add header file

--- Makefile
+++ Makefile
@@ -2,11 +2,12 @@ CC=gcc
 CFLAGS=-c -Wall
 LDFLAGS= -lcrypto -I/usr/include/libxml2 -lxml2
 SOURCES=main.c
+HEADERS=main.h
 OBJECTS=$(SOURCES:.c=.o)
 EXECUTABLE=main
 
 all: $(SOURCES) $(EXECUTABLE)
-	
+
 $(EXECUTABLE): $(OBJECTS)
 	$(CC) $(LDFLAGS) $(OBJECTS) -o $@
 
--- main.h
+++ main.h
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 #include <libxml/parser.h>
 #include <libxml/tree.h>
 #include <libxml/HTMLparser.h>
 #include <libxml/HTMLtree.h>
+
+
+/* Global varibles */
+char *site = NULL;
+char *dirName = NULL;
+
+/* Define functions */
+void parseError(void *ctx);
+void getSite(void);
+void getSource(char* address, char *filename);
+void parseDocument(xmlNode *node);
+void getImage(xmlChar *src);
+
+char *createSiteDirectories();
 
