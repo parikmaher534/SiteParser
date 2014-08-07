8cc0b1fdb81fb739c787d6cc6ccbb7f12f0bcc6c
Nikolay Podstrigan <parikmaher534@gmail.com>
2014-08-07 23:54:55 +0400

    Update parser. Parse return array of all tags

--- lib/parser.c
+++ lib/parser.c
@@ -2,7 +2,7 @@
 #include <string.h>
 
 
-void parseHTML(char* path, char* tag) {
+char** parseHTML(char* path, char* tag) {
 	FILE* page = fopen(path, "r");
 
 	//Get file size	
@@ -10,46 +10,62 @@ void parseHTML(char* path, char* tag) {
 	int fileSize = ftell(page);
 	fseek(page, 0L, SEEK_SET);
 
+	char** tags = malloc(fileSize);
+	int tagsIndex = 0;
+
+	char* tagStr = (char*)malloc(1);
 	int symbol = fgetc(page);
-	char out[fileSize];
 	int index = 0;
 	int search = 0;
-
+	
 	while( symbol != EOF ) {
 
 		//If we in search mode detect is it searching tag
 		if( search == 1 ) {
 			if( symbol == tag[index] ) {
-				out[index + 1] = symbol;
+				tagStr = (char*)realloc(tagStr, index + 1);
+				tagStr[index + 1] = symbol;
+
 				index++;
 			} else {
 				search = 0;
 				index = 0;
 			}
 			
-			if( strlen(tag) == index ) search = 2;
+			if( strlen(tag) == index ) {
+				search = 2;
+				index++;
+			}
 		} 
 
 		//If it was searching tag - get whole tag with all attributes
 		else if( search == 2 ) {
-			out[index + 1] = symbol;
-			
+			tagStr = (char*)realloc(tagStr, index + 2);
+			tagStr[index] = symbol;
+	
 			if( strrchr(">", symbol) == 0 ) {
 				index++;
 			} else {
+				char* tag = (char*)malloc(index + 2);
+				strcpy(tag, tagStr);
+
+				tags[tagsIndex] = tag;
 				search = 0;
 				index = 0;
+				tagsIndex++;
 			}
 		}
 
 		//If we find '<' enable tag search mode	
 		if( strrchr("<", symbol) != NULL ) {
-			out[index] = symbol;
+			tagStr[index] = symbol;
 			search = 1;
 		}
 		
 		symbol = fgetc(page);
 	}
 
-	printf("OUT: %s", out);
+	free(tagStr);
+
+	return tags;
 }
